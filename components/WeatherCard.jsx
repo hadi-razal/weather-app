a4cb344eeb4bb852fb44941c6a6c76c86aeeea62
hadi-razal <hadhirasal22@gmail.com>
2024-06-21 03:34:40 +0530

    updates in weather card

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -4,7 +4,10 @@ const WeatherCard = () => {
   return (
     <div className="z-10 text-primaryFontColor flex flex-col items-center justify-center gap-1 lg:h-[450px] lg:w-[400px] w-full bg-primaryColor rounded-2xl p-6 shadow-lg">
       <span className="text-2xl lg:text-3xl font-bold">Today</span>
-      <span className="text-[40px] lg:text-[60px] font-bold">46°C</span>
+      <div className="flex items-center justify-center gap-2">
+        <img src="/cloud.png" alt="Weather Icon" className="w-12 h-10" />
+        <span className="text-[40px] lg:text-[60px] font-bold">46°C</span>
+      </div>
       <span className="text-md lg:text-lg font-medium">Sunny</span>
       <span className="text-md lg:text-lg font-medium">Kochi, Kerala</span>
       <span className="text-xs lg:text-sm font-light">Date: 10/30/2003</span>
